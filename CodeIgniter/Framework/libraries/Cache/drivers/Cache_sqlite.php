c5e241bd1da579ca81497d21aeb00ceaf244f898
otengkwame <developerkwame@gmail.com>
2024-05-08 12:04:44 +0000

    :sparkles: add member methods
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- CodeIgniter/Framework/libraries/Cache/drivers/Cache_sqlite.php
+++ CodeIgniter/Framework/libraries/Cache/drivers/Cache_sqlite.php
@@ -69,7 +69,233 @@ class CI_Cache_sqlite extends CI_Driver {
     * @var PDO
     */
     protected $sqlite;
-
     
+    /**
+    * Constructor.
+    */
+    public function __construct() 
+    {
+       
+        $CI =& get_instance();
+        
+        // Get cache_path from config if available.
+        $path = $CI->config->item('cache_path');
+        $this->cache_path = ($path == '') ? WRITABLEPATH . 'cache/' : $path;
+        
+        // Get cache_file name from config if available.
+        $cache_file = $CI->config->item('cache_file');
+        $this->cache_file = ($cache_file == '') ? 'cache.sqlite' : $cache_file;
+        
+        // Get auto_flush name from config if available.
+        $auto_flush = $CI->config->item('cache_autoflush');
+        $this->auto_flush = ($auto_flush == '') ? false : $auto_flush;
+        
+        // Initialize the database.
+        try {
+            
+            $this->sqlite = new PDO('sqlite:' . $this->cache_path . $this->cache_file);
+            $this->sqlite->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // Create cache database.
+            $this->sqlite->exec("CREATE TABLE IF NOT EXISTS cache (id TEXT PRIMARY KEY, data BLOB, expire INTEGER)");
+            
+            // Don't verify data on disk.
+            $this->sqlite->exec("PRAGMA synchronous = OFF");
+
+            // Turn off rollback.
+            $this->sqlite->exec("PRAGMA journal_mode = OFF");
+
+            // Periodically clean the database.
+            $this->sqlite->exec("PRAGMA auto_vacuum = INCREMENTAL");
+        } catch ( PDOException $e ) {
+            show_error($e->getMessage());
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+    * Is supported
+    *
+    * Check if the SQLite PDO driver is available
+    * 
+    * @return boolean
+    */
+    public function is_supported() 
+    {
+        return in_array("sqlite", PDO::getAvailableDrivers());
+    }
+
+    // ------------------------------------------------------------------------
+   
+    /**
+    * Fetch from cache
+    *
+    * @param 	mixed		unique key id
+    * @return 	mixed		data on success/false on failure
+    */
+   public function get($id) 
+   {
+       try {
+
+           $query = $this->sqlite->query("SELECT * FROM cache WHERE id = '" . (string) $id . "'");
+           
+           // cache miss
+           if (!$query || !$data = $query->fetch(PDO::FETCH_ASSOC)) {
+               return false;
+           }
+
+           // time to live elapsed
+           if (time() > $data['expire']) {
+               $this->delete($id);
+               return false;
+           }
+           
+           return unserialize($data['data']);
+       } catch ( PDOException $e ) {
+           return false;
+        }
+    }
+
+   // ------------------------------------------------------------------------
+
+   /**
+    * Save into cache
+    *
+    * @param 	string		unique key
+    * @param 	mixed		data to store
+    * @param 	int			length of time (in seconds) the cache is valid 
+    * Default is 60 seconds
+    * @return 	boolean		true on success/false on failure
+    */
+   public function save($id, $data, $ttl = 60) 
+   {
+       try {
+           // insert or replace data
+           $query = $this->sqlite->query("INSERT OR REPLACE INTO cache(id, data, expire) VALUES ('" . (string) $id . "', '" . serialize($data) . "', '" . (time() $ttl) . "')");
+           
+           // trigger auto-flush
+           if ($this->auto_flush)
+               $this->flush();
+           
+           return $query ? true : false;
+       } catch ( PDOException $e ) {
+           return false;
+       }
+   }
+   
+   // ------------------------------------------------------------------------
+   
+
+   /**
+    * Delete from Cache
+    *
+    * @param 	mixed		unique identifier of item in cache
+    * @return 	boolean		true on success/false on failure
+    */
+   public function delete($id) 
+   {
+       try {
+           // delete data
+           $query = $this->sqlite->exec("DELETE FROM cache WHERE id = '" . (string) $id . "'");
+           return $query ? true : false;
+       } catch ( PDOException $e ) {
+           return false;
+       }
+   }
+   
+   // ------------------------------------------------------------------------
+
+   /**
+    * Clean the Cache
+    *
+    * @return 	boolean		false on failure/true on success
+    */
+   public function clean() 
+   {
+       try {
+           // delete all data
+           $query = $this->sqlite->exec("DELETE FROM cache");
+           return $query ? true : false;
+       } catch ( PDOException $e ) {
+           return false;
+       }
+   }
+   
+   // ------------------------------------------------------------------------
+
+   /**
+    * A custom method that will flush all expired cache items
+    *
+    * @return 	boolean		false on failure/true on success
+    */
+   public function flush() 
+   {
+       try {
+           $query = $this->sqlite->exec("DELETE FROM cache WHERE expire < '" . time() . "'");
+           return $query ? true : false;
+       } catch ( PDOException $e ) {
+           return false;
+       }
+   }
+   
+   // ------------------------------------------------------------------------
+   
+   /**
+    * Cache Info
+    *
+    * @return 	mixed 	false
+    */
+   public function cache_info() 
+   {
+       try {
+        
+           $info = array();
+           
+           // get number of items in cache
+           $query = $this->sqlite->query("SELECT count(1) FROM cache");
+
+           if ($query && $result = $query->fetch()) {
+               $info["items"] = $result[0];
+           } else {
+               $info["items"] = 0;
+           }
+
+           $info["size"] = filesize($this->cache_path . $this->cache_file);
+           $info["path"] = $this->cache_path;
+           $info["filename"] = $this->cache_file;
+           
+           return $info;
+
+       } catch ( PDOException $e ) {
+           return false;
+       }
+   }
+   
+   // ------------------------------------------------------------------------
+
+   /**
+    * Get Cache Metadata
+    *
+    * @param 	mixed		key to get cache metadata on
+    * @return 	mixed		false on failure, array on success.
+    */
+   public function get_metadata($id) 
+   {
+       try {
+
+           $query = $this->sqlite->query("SELECT * FROM cache WHERE id = '" . (string) $id . "'");
+           
+           // cache miss
+           if (!$query || !$data = $query->fetch(PDO::FETCH_ASSOC)) {
+               return false;
+           }
+
+           return $data;
+
+       } catch ( \PDOException $e ) {
+           return false;
+       }
+   }    
    
 }
