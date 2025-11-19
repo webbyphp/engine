49a82ae3f8edaeda39ed7b9aee88aed00d155720
otengkwame <developerkwame@gmail.com>
2025-11-19 22:56:29 +0000

    :construction: wip
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- Core/core/Helpers/Autoloader.php
+++ Core/core/Helpers/Autoloader.php
@@ -16,52 +16,51 @@ class Autoloader
      * @var string Nampsapce prefix refered to App Root
      */
     const string DEFAULT_PREFIX = "App";
-    
-    public static function autoload($classe): void 
-    { 
-        $namespace= static::getPrincipalNamespace($classe);
-        
-        $namespace = APPROOT. $namespace . '.php';
+
+    public static function autoload($classe): void
+    {
+        $namespace = static::getPrincipalNamespace($classe);
+
+        $namespace = APPROOT . $namespace . '.php';
         $namespace = str_replace('\\', DS, $namespace);
-    
-        if (! file_exists($namespace) || is_dir($namespace)) { 
-            throw new \Exception("******* This Class don't exists!! --->>>> ".$namespace. "*********\n");
+
+        if (! file_exists($namespace) || is_dir($namespace)) {
+            throw new \Exception("******* This Class don't exists!! --->>>> " . $namespace . "*********\n");
         }
-    
+
         require $namespace;
     }
-    
-    protected static function getPrincipalNamespace($namespace): ?string 
-    { 
+
+    protected static function getPrincipalNamespace($namespace): ?string
+    {
         $configs = (array) static::getConfigIfExists();
 
-        if(!isset($configs)) { 
-            return  $namespace;    
+        if (!isset($configs)) {
+            return  $namespace;
         }
 
         $principalNamespace = explode('\\', $namespace)[0];
-       
+
         foreach ($configs as $key => $value) {
             $keyReplaced = str_replace('\\', '', $key);
-    
+
             if ($principalNamespace == $keyReplaced) {
                 return str_replace($key, $value, $namespace);
             }
         }
-    
+
         return $namespace;
     }
-    
-    protected static function getConfigIfExists(): ?object 
-    { 
+
+    protected static function getConfigIfExists(): ?object
+    {
         $maping = static::getWebbyJsonToObject();
         return $maping->autoload ?? null;
-    } 
-    
-    protected static function getWebbyJsonToObject(): ?object 
-    { 
+    }
+
+    protected static function getWebbyJsonToObject(): ?object
+    {
         $file = ROOTPATH . 'webby.json';
         return (file_exists($file)) ? json_decode(file_get_contents($file)) : null;
     }
-    
 }
